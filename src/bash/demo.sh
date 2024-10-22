94daf6a4690af2017e001b0a0f9ac1cea99cf62f
apicis <tommaso.apicella95@gmail.com>
2024-10-22 08:12:37 +0100

    Upload bash files

--- src/bash/demo.sh
+++ src/bash/demo.sh
@@ -1,10 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 GPU_ID=0
-MODEL_NAME=AffNet
-TRAIN_DATA=UMD
-DATA_DIR=/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing
-CKPT_PATH=/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_AffNet/affnet_40.pth
+MODEL_NAME="AffNet"
+TRAIN_DATA="UMD"
+DATA_DIR="/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing"
+# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_CNN/cnn_54.pth"
+# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_DRNAtt/drnatt_44.pth"
+CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_AffNet/affnet_40.pth"
+# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_Mask2Former/mask2former_89.pth"
 BATCH_SIZE=1
 VIS_OVERLAY=True
 
@@ -20,4 +23,10 @@ python src/demo.py \
 #--save_res=True\
 #--save_overlay=True
 #--dest_dir=
+
+#python src/demo.py --gpu_id=0 --model_name=DRNAtt \
+#--train_dataset=UMD \
+#--data_dir=/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing \
+#--checkpoint_path=/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_DRNAtt/drnatt_44.pth \
+#--batch_size=1
 
