02c4085601ab7530645929f0e923931ff6774eb6
apicis <tommaso.apicella95@gmail.com>
2024-10-26 15:15:16 +0100

    Initialised bash scripts

--- src/bash/demo.sh
+++ src/bash/demo.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 GPU_ID=0
-MODEL_NAME="AffNet"
-TRAIN_DATA="UMD"
-DATA_DIR="/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing"
-# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_CNN/cnn_54.pth"
-# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_DRNAtt/drnatt_44.pth"
-CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_AffNet/affnet_40.pth"
-# CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_Mask2Former/mask2former_89.pth"
+MODEL_NAME="..."
+TRAIN_DATA="..."
+DATA_DIR="...."
 BATCH_SIZE=1
 VIS_OVERLAY=True
 
@@ -18,15 +14,7 @@ python src/demo.py \
 --data_dir=$DATA_DIR \
 --checkpoint_path=$CKPT_PATH \
 --batch_size=$BATCH_SIZE \
---visualise_overlay=$VIS_OVERLAY
-
-#--save_res=True\
-#--save_overlay=True
-#--dest_dir=
-
-#python src/demo.py --gpu_id=0 --model_name=DRNAtt \
-#--train_dataset=UMD \
-#--data_dir=/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing \
-#--checkpoint_path=/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_DRNAtt/drnatt_44.pth \
-#--batch_size=1
-
+--visualise_overlay=$VIS_OVERLAY # \
+#--save_overlay=$SAVE_OVERLAY \
+#--save_res=$SAVE_RES \
+#--dest_dir=$DEST_DIR
--- src/bash/demo_all.sh
+++ src/bash/demo_all.sh
@@ -6,33 +6,28 @@ VIS_OVERLAY=False
 SAVE_RES=True
 SAVE_OVERLAY=True
 TRAIN_DATA="UMD"
-DATA_DIR="/mnt/storage/Tommaso/part-affordance-dataset-tools/part-affordance-dataset/data/testing"
-DEST_DIR_BASE="/mnt/storage/Tommaso/affseg_release_test/UMD/"
-MODEL_NAME="CNN"
+DATA_DIR="..."
+DEST_DIR_BASE="..."
+CKPT_DIR_BASE="..."
+
 for MODEL_NAME in "CNN" "DRNAtt" "AffNet" "Mask2Former"
 do
-  # echo $MODEL_NAME
   if [ "$MODEL_NAME" = "CNN" ]
   then
-    # echo "first"
-    CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_CNN/cnn_54.pth"
+    CKPT_PATH="$CKPT_DIR_BASE/UMD_CNN/cnn_54.pth"
   elif [[ "$MODEL_NAME" == "DRNAtt" ]]
   then
-    # echo "second"
-    CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_DRNAtt/drnatt_44.pth"
+    CKPT_PATH="$CKPT_DIR_BASE/UMD_DRNAtt/drnatt_44.pth"
   elif [[ "$MODEL_NAME" == "AffNet" ]]
   then
-    # echo "third"
-    CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_AffNet/affnet_40.pth"
+    CKPT_PATH="$CKPT_DIR_BASE/UMD_AffNet/affnet_40.pth"
   elif [[ "$MODEL_NAME" == "Mask2Former" ]]
   then
-    #echo "fourth"
-    CKPT_PATH="/mnt/storage/Tommaso/aff-seg/checkpoints/UMD/UMD_Mask2Former/mask2former_89.pth"
+    CKPT_PATH="$CKPT_DIR_BASE/UMD_Mask2Former/mask2former_89.pth"
   fi
-  # echo "launch script"
+
   DEST_DIR="$DEST_DIR_BASE$MODEL_NAME"
-#  echo "$DEST_DIR"
-# python src/demo.py --gpu_id=$GPU_ID --model_name=$MODEL_NAME --train_dataset=$TRAIN_DATA --data_dir=$DATA_DIR --checkpoint_path=$CKPT_PATH --batch_size=$BATCH_SIZE --save_res=$SAVE_RES --save_overlay=$SAVE_OVERLAY --dest_dir=$DEST_DIR
+
   python src/demo.py \
   --gpu_id=$GPU_ID \
   --model_name=$MODEL_NAME \
